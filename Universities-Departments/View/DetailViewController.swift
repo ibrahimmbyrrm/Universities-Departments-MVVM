4be2ec9c7f362799ff6b6499f6e97f57dbe00887
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-02-21 13:22:59 +0300

    ff

---
+++ Universities-Departments/View/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Universities-Departments
+//
+//  Created by İbrahim Bayram on 21.02.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    static var selectedUni : University?
+    @IBOutlet weak var testLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
